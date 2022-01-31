bf6ff3a53ae59fa88f7ea4d3763d50ccca78ad08
viznev <viznev@users.noreply.github.com>
2022-01-31 06:26:05 +0000

    second solution

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,45 +1,41 @@
-def arithmetic_arranger(problems, displayAnswer=False):
-  num1List = []
-  num2List = []
-  operatorList = []
-  answersList = []
-  if len(problems) > 5:
-    return "Error: Too many problems."
-  for problem in problems:
-    problemSplit = problem.split()
-    operator = problemSplit[1]
-    num1 = problemSplit[0]
-    num2 = problemSplit[2]
-    if operator=='+' or operator=='-':
-      if num1.isdecimal() and num2.isdecimal():
-        if len(num1) <= 4 and len(num2) <= 4:
-          num1List.append(num1)
-          num2List.append(num2)
-          operatorList.append(operator)
-          answersList.append(str(int(num1)+int(num2)) if operator=='+' else str(int(num1)-int(num2)))
-        else:
-          return "Error: Numbers cannot be more than four digits."
-      else:
-          return "Error: Numbers must only contain digits."
-    else:
-      return "Error: Operator must be '+' or '-'."
-  # arrange the problems
-  line1=''
-  line2=''
-  line3=''
-  line4=''
-  problemSpacing=''
-  for i in range(len(num1List)):
-    if i == 1:
-      problemSpacing = '    '
-    adjustment = len(num2List[i])+2 if len(num2List[i]) > len(num1List[i]) else len(num1List[i])+2
-    line1 += problemSpacing + num1List[i].rjust(adjustment)
-    line2 += problemSpacing + operatorList[i] + ' ' + num2List[i].rjust(adjustment-2)
-    line3 += problemSpacing + '-' * adjustment
-    line4 += problemSpacing + answersList[i].rjust(adjustment)
+from math import perm
 
-  arranged_problems = line1 + '\n' + line2 + '\n' + line3
-  if displayAnswer == True:
-    arranged_problems += '\n' + line4
-    
+
+def arithmetic_arranger(problems, displayAnswer=False):
+  # setup variables
+  arranged_problems = ''
+  permittedOperators = ['+', '-']
+  line_topOperands = ''
+  line_operatorsAndBottomOperands = ''
+  line_dashes = ''
+  line_answers = ''
+  firstProblem = True
+
+  # return Error if the number of problems given is greater than 5
+  if len(problems) > 5: return "Error: Too many problems."
+
+  # separate each problem's string into [operand, operator, operand] and store each set in a list
+  challenges = list(map(lambda x: x.split(), problems))
+
+  # iterate through each problem and validate data
+  for challenge in challenges:
+    # return Error if any of the operators are not a permitted operator
+    if not any(item in permittedOperators for item in challenge[1]): return "Error: Operator must be '+' or '-'."
+    # return Error if any of the operands are not digits (0-9)
+    if not all(item.isdecimal() for item in [challenge[0], challenge[2]]): return "Error: Numbers must only contain digits."
+    # return Error if any of the operands are greater than four numbers
+    if not all(len(item) <= 4 for item in [challenge[0], challenge[2]]): return "Error: Numbers cannot be more than four digits."
+
+    # data has been validated, now perform the operation and format it
+    spaceAdjustment = max(len(challenge[0]), len(challenge[2])) + 2
+    problemSpacing = '' if firstProblem else ' ' * 4
+    line_topOperands += problemSpacing + challenge[0].rjust(spaceAdjustment)
+    line_operatorsAndBottomOperands += problemSpacing + challenge[1] + challenge[2].rjust(spaceAdjustment-1)
+    line_dashes += problemSpacing + '-' * spaceAdjustment
+    line_answers += problemSpacing + str(eval(''.join(challenge))).rjust(spaceAdjustment)
+    firstProblem = False
+
+  arranged_problems = line_topOperands + '\n' + line_operatorsAndBottomOperands + '\n' + line_dashes
+  if displayAnswer == True: arranged_problems += '\n' + line_answers
+
   return arranged_problems
